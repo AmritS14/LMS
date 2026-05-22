c4f5bd10156199e9b1a5244b867c710d6da6229b
Aadya Tiwari <aadyatiwari01@gmail.com>
2026-05-22 16:29:29 +0530

    Delete Shared directory

--- Shared/Services/MessagingService.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-protocol MessagingService: Sendable {
-    func threads(for userID: UUID) async throws -> [MessageThread]
-    func messages(threadID: UUID) async throws -> [ChatMessage]
-    func send(_ message: ChatMessage) async throws -> ChatMessage
-    func markRead(threadID: UUID, upTo: Date) async throws
-}
